1f90a5cfee7ccb39d8e3e185ec8a7f2bd8728c1d
praveen <praveen@localhost>
2020-01-04 22:02:44 -0500

    -created new hero-detail component structure and hero-service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from './hero';
-import {HEROES} from '../mock-heroes';
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
 
 @Component({
     selector: 'app-heroes',
@@ -9,12 +9,12 @@ import {HEROES} from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-    heroes:Hero = [];
+    heroes:Hero[] = [];
 
-    constructor() { }
+    constructor(private heroService: HeroService) { }
 
     ngOnInit() {
-        this.heroes = HEROES;
+        this.heroes = this.heroService.getHeroes();
     }
     selectedHero: Hero;
     selectHero(hero:Hero):void {
